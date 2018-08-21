4d9c9d0dbb1ebb55c9ede5b8f00a61f7fe542e51
7R41N33 <chepenkoanton2011@yandex.ru>
2018-08-21 10:25:41 +0400

    fp rating and chars count

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,15 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        films = array.select { |f| f['rating_kinopoisk'].to_f > 0 && f['country'].to_s.split(',').size > 1 }
+        ratings = films.reduce(0) { |a, e| a + e['rating_kinopoisk'].to_f }
+        ratings / films.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.select { |f| f['rating_kinopoisk'].to_f >= threshold }
+             .reduce(0) { |a, e| a + e['name'].chars.select { |c| c.eql?('и') }.size }
       end
     end
   end
